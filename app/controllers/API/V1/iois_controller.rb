44dca9e1dd3a25499922dd4d50c711ec4f2f12db
kylewstewart <kyle.w.stewart@mac.com>
2017-07-01 19:53:28 -0400

    need to complete ioi destroy

--- app/controllers/API/V1/iois_controller.rb
+++ app/controllers/API/V1/iois_controller.rb
@@ -7,25 +7,29 @@ class Api::V1::IoisController < ApplicationController
   end
 
   def create
-    ranked_agent_ids = params['IOI']['rankedAgents'].map{|name| Agent.find_by(name: name).id}
+    ranked_agent_ids = params['IOI']['rankedAgents']
+      .map{|name| Agent.find_by(name: name).id}
     stock_id = Stock.find_by(exch_code: params['IOI']['stock']).id
     side = params['IOI']['side']
-    principal_id = params['principal_id'].to_iw      
-
+    principal_id = params['principal_id']
     ioi = Ioi.create(principal_id: principal_id, ranked_agent_ids: ranked_agent_ids,
-        stock_id: stock_id, side: params['IOI']['side'])
-
+        stock_id: stock_id, side: params['IOI']['side'], active: true)
     render json: ioi
   end
 
   def update
     ioi = Ioi.find(params['IOI']['id'])
-    ranked_agent_ids = params['IOI']['rankedAgents'].map{|name| Agent.find_by(name: name).id}
+    ranked_agent_ids = params['IOI']['rankedAgents']
+      .map{|name| Agent.find_by(name: name).id}
     stock_id = Stock.find_by(exch_code: params['IOI']['stock']).id
     side = params['IOI']['side']
-
     ioi.update(ranked_agent_ids: ranked_agent_ids, stock_id: stock_id, side: side)
+    render json: ioi
+  end
 
+  def destroy
+    ioi = Ioi.find(params['id'])
+    ioi.update(active: false)
     render json: ioi
   end
 
