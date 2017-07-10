4fd2a88d259582ff4b474594f69461cc74b8a7db
kylewstewart <kyle.w.stewart@mac.com>
2017-07-10 19:05:59 -0400

    started adding comments to matching algorithm

--- app/models/negotiation.rb
+++ app/models/negotiation.rb
@@ -3,7 +3,9 @@ class Negotiation < ApplicationRecord
   has_many :negotiation_principals
   has_many :principals, through: :negotiation_principals
 
-
+  ##  MATCH
+  #   Calls #get_matches to find all matches from IOIs in DB. Returns false if none exsist.
+  #   Calls #get_negotiations to find and create new Negotiations from the set of Matches. Returns false if none exist.
   def self.match
     matches = get_matches
     return false if !matches
@@ -12,13 +14,22 @@ class Negotiation < ApplicationRecord
     negotiations
   end
 
+  ##  GET_MATCHES
+  #   Retreives all active IOIs from DB and groups by stock.
+  #   Adds IOIs, grouped by stock, to Matches when there are at least one buy and one sell IOI
   def self.get_matches
     iois_by_stk = Ioi.where(active: true).group_by(&:stock_id)
-    matches = iois_by_stk.select{|stk_id, iois|iois.select{|ioi|ioi.side=="Buy"}
-      .count!=0 && iois.select{|ioi|ioi.side=="Sell"}.count!=0}
+    matches = iois_by_stk.select{|stk_id, iois|iois.select{|ioi|ioi.side=="Buy"}.count!=0 && iois.select{|ioi|ioi.side=="Sell"}.count!=0}
     matches.empty? ? false : matches
   end
 
+  ##  GET_NEGOTIATIONS
+  #   Iterates over array of Matches
+  #   Calls #get_pref_broker to find the most common preferred broker for each match. Return false if none exisit.
+  #   Creates a new instance of a Negotiation that belongs to the preferred broker.
+  #   Calls #add_particitpants to create an association between Principals and a Negotiation.
+  #   Pushes the new Negotiation to the array Negotiations.
+  #   Returns false if Negiations is empty
   def self.get_negotiations(matches)
     negotiations = []
     matches.each do |stock_id, iois|
@@ -31,15 +42,30 @@ class Negotiation < ApplicationRecord
     !negotiations.empty? ? negotiations : false
   end
 
+  ##  GET_PREF_BROKER
+  #   Calls #get_common_brokers to find the most common broker.
+  #   Returns false if no common broker exists
+  #   Iterates over the the iois in a match and collects an array of broker preferance lists.
+  #   Calls #ranked_voting to find and return the preferred most common broker.
   def self.get_pref_broker(iois)
-    common = self.get_common_brokers(iois)
+    common = self.get_common_broker(iois)
     return false if !common
     ranked_agents = iois.map{|ioi| ioi.ranked_agent_ids}
     self.ranked_voting(ranked_agents, common)
   end
 
-  def self.get_common_brokers(iois)
-    freq = iois.map{|ioi|ioi.ranked_agent_ids}.flatten.inject(Hash.new(0)){|h,v|h[v] += 1;h}
+  ##  GET_COMMON_BROKER
+  #   Collects the preferred broker lists (ranked_agent_ids) from each IOI into one array
+  #   Creates a hash of key: broker(agent_id) and value: number of occurances.
+  #   Interates over the range of the count of all brokers to 2.
+  #     [Brokers count = max occurance and 2 = min occurance (must have at least on buyer and one seller)]
+  #     1. Selects broker if occurance is equal to count
+  #     2. Calls #remove_no_buyer_or_no_seller to check that at least one buyer and one seller is present
+  #     3. Breaks loop iteration if 1 & 2 are true
+  #   Returns false if no common broker is found.
+  def self.get_common_broker(iois)
+    all_brokers = iois.map{|ioi|ioi.ranked_agent_ids}.flatten
+    freq = all_brokers.inject(Hash.new(0)){|h,v|h[v] += 1;h}
     count = iois.count
     while count > 1 do
       common = freq.select{|k,v| v == count}
@@ -49,6 +75,9 @@ class Negotiation < ApplicationRecord
     end
     common.empty? ? false : common.map{|k,v| k}
   end
+
+  ##  REMOVE_NO_BUYER_OR_NO_SELLER
+  #   
 
   def self.remove_no_buyer_or_no_seller(common, iois)
     return common if common.empty?
