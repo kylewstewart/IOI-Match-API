c2447d185ccb6eca68c3d0036590ba5b266077b7
kylewstewart <kyle.w.stewart@mac.com>
2017-07-27 16:35:23 -0400

    fixed update rating bug

--- app/serializers/negotiation_principal_serializer.rb
+++ app/serializers/negotiation_principal_serializer.rb
@@ -1,5 +1,5 @@
 class NegotiationPrincipalSerializer < ActiveModel::Serializer
-  attributes :id, :name, :side, :rating, :traded, :negotiation_id
+  attributes :id, :name, :side, :rating, :traded, :negotiation_id, :principal_id
 
   def name
     Principal.find(object.principal_id).name
